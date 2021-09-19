c6359d1ea5f47d38c88f949c9f275e341dc104ec
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-09-19 17:37:33 +0300

    order class

---
+++ src/model/Order.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Order {
+	
+	private Customer customer;
+	private int id;
+	private ArrayList<OrderItem> items;
+	private Date orderDate;
+	private double orderTotal;
+	
+	public Order() {
+		
+	}
+
+	public Order(Customer customer, int id, ArrayList<OrderItem> item, Date orderDate, double orderTotal) {
+		super();
+		this.items = new ArrayList<OrderItem>();
+		this.customer = customer;
+		this.id = id;
+		this.orderDate = orderDate;
+		this.orderTotal = orderTotal;
+	}
+	
+	// insert order item
+	public void addOrderItem(OrderItem item) {
+		items.add(item);
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public ArrayList<OrderItem> getItems() {
+		return items;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public double getOrderTotal() {
+		return orderTotal;
+	}
+	
+	
+
+}
